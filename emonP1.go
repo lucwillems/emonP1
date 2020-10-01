41feee35b75af190445adfca6fb995d2e6437001
luc willems <luc.willems@t-m-m.be>
2020-10-01 22:04:04 +0200

    - initial import

---
+++ emonP1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/skoef/gop1"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	message, err := ioutil.ReadFile("test/data")
+	if (err!=nil) {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(message), "\n")
+	telegram :=gop1.ParseTelegram(lines);
+	fmt.Fprintf(os.Stdout,"Device: %s\n",telegram.Device);
+	fmt.Fprintf(os.Stdout,"version: %s\n",telegram.Version);
+	fmt.Fprintf(os.Stdout,"Objects: %d\n",telegram.Size());
+	fmt.Fprintf(os.Stdout,"timestamp: %s\n",telegram.Timestamp.Format(time.RFC3339));
+
+	for _,v := range telegram.Objects {
+		fmt.Println(v.ToString())
+	}
+
+	fmt.Fprintf(os.Stdout,"%s\n",telegram.Get(gop1.OBISTypeElectricityDelivered).ToString());
+	if f,err:=telegram.Get(gop1.OBISTypeInstantaneousCurrentL1).AsFloat();err==nil {
+		fmt.Fprintf(os.Stdout,"float: %.2f\n",f);
+
+	} else {
+		fmt.Fprint(os.Stderr,err);
+	}
+}
+
---
+++ go.mod
@@ -0,0 +1,11 @@
+module emonP1
+
+require (
+	github.com/skoef/gop1 v0.0.0
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+)
+
+replace github.com/skoef/gop1 => ./gop1
+
+go 1.15
