a7447d9bd7062f2dfdc1fa5df4f8b7229d52963a
luc willems <luc.willems@t-m-m.be>
2020-10-17 19:09:59 +0100

    - copy to local emonPi for local testing

--- build-pi.sh
+++ build-pi.sh
@@ -5,3 +5,5 @@ env GOARCH=$ARCH GOOS=linux go build  -buildmode exe -o emonP1-pi.$ARCH emonP1.g
 file emonP1-pi.$ARCH
 
 scp -P 2222 emonP1-pi.$ARCH root@81.165.73.9:/tmp
+scp         emonP1-pi.$ARCH root@192.168.0.251:/tmp
+
