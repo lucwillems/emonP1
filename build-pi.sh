bef6d6a090b69da275e8fadf20bd5140f0346391
Luc Willems <luc.willems@it4y.eu>
2021-03-12 12:50:55 +0100

    Update build-pi.sh

--- build-pi.sh
+++ build-pi.sh
@@ -3,7 +3,3 @@ ARCH=arm
 echo "build for raspberry PI"
 env GOARCH=$ARCH GOOS=linux go build  -buildmode exe -o emonP1-pi.$ARCH emonP1.go
 file emonP1-pi.$ARCH
-
-scp -P 2222 emonP1-pi.$ARCH root@81.165.73.9:/tmp
-scp         emonP1-pi.$ARCH root@192.168.0.251:/tmp
-
